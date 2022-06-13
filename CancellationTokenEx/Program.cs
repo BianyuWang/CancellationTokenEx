2afc7b486d0e6e62c0651e2a2d70d6c6cd063b79
Bian Yu Wang <bianyu.wang@gmail.com>
2022-06-13 11:19:38 -0400

    way 3,  pass cancellationToken to httpclient

--- CancellationTokenEx/Program.cs
+++ CancellationTokenEx/Program.cs
@@ -16,21 +16,41 @@ namespace CancellationTokenEx
             cts.CancelAfter(5000); // pass milliseconds
 
             // download google 100 times, and cancel download after 5 seconds, if download is not finished.
+
+            // way 1, 
+
+            await Download1Async(url, 100, cts.Token);
+
+            //way 2
             try
             {
-                await DownloadAsync(url, 100, cts.Token);
+                await Download2Async(url, 100, cts.Token);
                 //await DownloadAsync(url, 100);
             }
             catch (Exception e)
             {
                 if (cts.IsCancellationRequested)
                 { Console.WriteLine("request is cancelled"); }
-              
+
             }
 
+            //way 3 pass cancellationToken to GetAsync
+
+            try
+            {
+                await Download3Async(url, 100, cts.Token);
+                //await DownloadAsync(url, 100);
+            }
+            catch (Exception e)
+            {
+                if (cts.IsCancellationRequested)
+                { Console.WriteLine("request is cancelled"); }
+
+            }
+          
 
         }
-        static async Task DownloadAsync(string URL, int repeat, CancellationToken cancellationToken=default)
+        static async Task Download1Async(string URL, int repeat, CancellationToken cancellationToken=default)
         {
             // to clean unmanaged resources, HttpClient has dispose method.
             using (HttpClient client = new HttpClient())
@@ -40,22 +60,59 @@ namespace CancellationTokenEx
                     string htmlText = await client.GetStringAsync(URL);
                     Console.WriteLine($"{DateTime.Now}:{htmlText}");
 
-                    #region Using cancellationToken way one
+                
                     // #way 1: check cancellationToken manually 
 
-                    //if (cancellationToken.IsCancellationRequested)
-                    //{
-                    //    // if cancelled, do something
-                    //    Console.WriteLine("Resquest is cancelled");
-                    //    break;
-                    //}
-                    #endregion
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        // if cancelled, do something
+                        Console.WriteLine("Resquest is cancelled");
+                        break;
+                    }
+                  
 
-                    //#way 2: throw an exception, and manage exception outside of function.
-                    cancellationToken.ThrowIfCancellationRequested();
+                   
                 }
             
             
+            }
+        }
+
+        static async Task Download2Async(string URL, int repeat, CancellationToken cancellationToken = default)
+        {
+            // to clean unmanaged resources, HttpClient has dispose method.
+            using (HttpClient client = new HttpClient())
+            {
+                for (var i = 0; i < repeat; i++)
+                {
+                    string htmlText = await client.GetStringAsync(URL);
+                    Console.WriteLine($"{DateTime.Now}:{htmlText}");
+
+                 
+                    //#way 2: throw an exception, and manage exception outside of function.
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+
+
+            }
+        }
+
+        static async Task Download3Async(string URL, int repeat, CancellationToken cancellationToken = default)
+        {
+            // to clean unmanaged resources, HttpClient has dispose method.
+            using (HttpClient client = new HttpClient())
+            {
+                for (var i = 0; i < repeat; i++)
+                {
+                    //pass cancellationtoken to getAsync
+                    var resp = await client.GetAsync(URL, cancellationToken);
+                    string htmlText = await resp.Content.ReadAsStringAsync();
+                    Console.WriteLine($"{DateTime.Now}:{htmlText}");
+
+
+                }
+
+
             }
         }
     }
