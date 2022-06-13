3949f15109551c7c906b9c270039bac92127e7c5
Bian Yu Wang <bianyu.wang@gmail.com>
2022-06-13 10:20:29 -0400

    CancellationToken example

--- CancellationTokenEx/Program.cs
+++ CancellationTokenEx/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CancellationTokenEx
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string url = "https://www.google.com";
+            CancellationTokenSource cts = new CancellationTokenSource();
+            //canel request after 5 seconds
+            cts.CancelAfter(5000); // pass milliseconds
+            
+            // download google 100 times, and cancel download after 5 seconds, if download is not finished.
+            await DownloadAsync(url, 100, cts.Token);
+            //await DownloadAsync(url, 100);
+
+
+        }
+        static async Task DownloadAsync(string URL, int repeat, CancellationToken cancellationToken=default)
+        {
+            // to clean unmanaged resources, HttpClient has dispose method.
+            using (HttpClient client = new HttpClient())
+            {
+                for (var i = 0; i < repeat; i++)
+                {
+                    string htmlText = await client.GetStringAsync(URL);
+                    Console.WriteLine($"{DateTime.Now}:{htmlText}");
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Resquest is cancelled");
+                        break;
+                    }
+                
+                }
+            
+            
+            }
         }
     }
 }
